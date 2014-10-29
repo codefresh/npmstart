4f09cf9219fe7ad2baaeec9c7f5d2b4ce4e8322c
Oleg verhovsky <verchol@Olegs-MacBook-Air.local>
2014-10-29 00:18:54 -0700

    fix redirect

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 router.get('/samples/:sample', function(req, res) {
-  console.log("in samples router");
-  proxy.web(req, res, { target: 'http://54.183.236.117:8081' });
+    console.log("in samples router");
+    proxy.web(req, res, { target: 'http://localhost:8081' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-
-/* GET users listing. */
-router.get('/samples/:sample', function(req, res) {
-  console.log("in samples router");
-  proxy.web(req, res, { target: 'http://localhost:80' });
-});
+ 
 
 module.exports = router;
