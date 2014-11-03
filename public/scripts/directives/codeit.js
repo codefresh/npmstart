70a55d9ff7de6ccd43f0eebb10e6829259d9526e
elartix <elartix@gmail.com>
2014-11-03 12:30:24 +0200

    - refactoring, remove unused dependencies

--- public/scripts/directives/codeit.js
+++ public/scripts/directives/codeit.js
@@ -1,63 +1,58 @@
 var app = angular.module('npmStartApp');
 app.directive('codeit', ['$http', 'cfpLoadingBar', function ($http, cfpLoadingBar) {
-  var spinnerId = 1;
+    var spinnerId = 1;
 
-  var directive = {
-    restrict: 'AE',
-    controller: controller,
-    template: '<button type="button" class="btn btn-default" ng-click="codeit()">{{codeButton.title}}</button>'
+    var directive = {
+        restrict: 'AE',
+        controller: controller,
+        template: '<button type="button" class="btn btn-default" ng-click="codeit()">{{codeButton.title}}</button>'
 
-  };
-  //<p><a href="{{deploy.url}}" style="z-index:1001;background-color: blue;" target="_blank">{{deploy.title}}</a></p>
+    };
+    //<p><a href="{{deploy.url}}" style="z-index:1001;background-color: blue;" target="_blank">{{deploy.title}}</a></p>
 //  <button type="button" class="btn btn-default" data-dismiss="modal">
 
-  return directive;
-  function controller($scope)
-  {
-      var codeButton  = {};
-      $scope.codeButton = codeButton;
-      codeButton.title = "Code It";
-      codeButton.status = "init";
+    return directive;
+    function controller($scope) {
+        var codeButton = {};
+        $scope.codeButton = codeButton;
+        codeButton.title = "Code It";
+        codeButton.status = "init";
 
 
-      $scope.codeit = function()
-      {
-        if (codeButton.status === "ready")
-          {
-            window.open(codeButton.url, "_blank");
-          }
-        cfpLoadingBar.start();
-        codeButton.title = "Setting environment ...";
-        codeButton.status = "inprogress";
+        $scope.codeit = function () {
+            if (codeButton.status === "ready") {
+                window.open(codeButton.url, "_blank");
+            }
+            cfpLoadingBar.start();
+            codeButton.title = "Setting environment ...";
+            codeButton.status = "inprogress";
 
 
-        $http.post('/env').success(function(data)
-        {
-            codeButton.status = "ready";
-            $scope.deploy = data;
-            $scope.deploy.title = "workspace is ready";
-            cfpLoadingBar.complete();
-            $scope.codeButton.title = "Ready to code!"
-            codeButton.url = data.url;
+            $http.post('/env').success(function (data) {
+                codeButton.status = "ready";
+                $scope.deploy = data;
+                $scope.deploy.title = "workspace is ready";
+                cfpLoadingBar.complete();
+                $scope.codeButton.title = "Ready to code!"
+                codeButton.url = data.url;
 
-        }).error(function(data)
-        {
-          alert("error: " + data);
+            }).error(function (data) {
+                alert("error: " + data);
+            });
+        }
+    }
+
+    function link(scope, element, attr) {
+        var spinner = new Spinner(opts);
+        var spinnerTarget = element.children();
+        var textElement = spinnerTarget.children();
+
+
+        element.on('click', handler);
+
+        scope.$on('$destroy', function () {
+            element.off('click', handler);
         });
-      }
-  }
-  function link(scope, element, attr) {
-    var spinner = new Spinner(opts);
-    var spinnerTarget = element.children();
-    var textElement = spinnerTarget.children();
-
-
-
-    element.on('click', handler);
-
-    scope.$on('$destroy', function() {
-      element.off('click', handler);
-    });
-  }
+    }
 
 }]);
