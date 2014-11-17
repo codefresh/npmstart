6a3a38c35629dfda963f519154988fc6e822add9
elartix <elartix@gmail.com>
2014-11-17 17:01:47 +0200

    - added npmstart provider

--- public/scripts/services/codeSampleService.js
+++ public/scripts/services/codeSampleService.js
@@ -17,8 +17,9 @@ app.service('CodeSampleService', ['$http', '$resource', function($http, $resourc
 
 	var getSampleProviders = function( ) {
 		var result = [
-			{name:'GitHub', type: 'github', icon: 'github', apiUrl:'213'},
-			{name:'Sourcegraph', type: 'sourcegraph', icon: 'sourcegraph', apiUrl:'376'},
+			{name:'NPMStart', type: 'npmstart', icon: 'cfs-npmstart', apiUrl:'212', enabled: true},
+			{name:'GitHub', type: 'github', icon: 'fa-github', apiUrl:'213', enabled: false},
+			{name:'Sourcegraph', type: 'sourcegraph', icon: 'sourcegraph', apiUrl:'376', enabled: false},
 		];
 		return result;
 	}
